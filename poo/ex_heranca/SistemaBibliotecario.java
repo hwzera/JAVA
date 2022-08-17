6049ef5ccf1b3d5e17eb0fa94207b2e896e0f213
hwzera <110138267+hwzera@users.noreply.github.com>
2022-08-16 21:50:29 -0400

    Add files via upload

---
+++ poo/ex_heranca/SistemaBibliotecario.java
@@ -0,0 +1,21 @@
+package poo.ex_heranca;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SistemaBibliotecario {
+    public static void main(String[] args) {
+        
+        int op = 0;
+
+        List<Funcionario> funcionarios = new ArrayList<Funcionario>();
+        List<Autor> autor = new ArrayList<Autor>();
+        List<Livro> livros = new ArrayList<Livro>();
+
+        do{
+            System.out.println("----- SISTEMA BIBLIOTECA -----");
+
+
+        } while (op != 0);
+    }
+}
