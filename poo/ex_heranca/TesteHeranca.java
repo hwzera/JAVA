7fbd7b85121ea7718f9aaf63b3c14a35a3f2d534
hwzera <110138267+hwzera@users.noreply.github.com>
2022-08-16 21:49:07 -0400

    Add files via upload

--- poo/ex_heranca/TesteHeranca.java
+++ poo/ex_heranca/TesteHeranca.java
@@ -22,7 +22,7 @@ public class TesteHeranca {
         LocalDate dataLancamentoNestJS = LocalDate.of(2014, Month.APRIL, 8);
         livroNestJS.setDtLancamento(dataLancamentoNestJS);
 
-        //Objeto Funcionario
+        //Objeto Func  = camila
         Funcionario func = new Funcionario();
         func.setNome("Camila");
         func.setEndereco("Rua n asfaltada");
@@ -33,16 +33,26 @@ public class TesteHeranca {
         func.setCargo("Faxineira");
         func.setMatricula("123456789");
 
-        //Obj Estudante
+        //Obj Estudante1 = Henrique
         Estudante estudante1 = new Estudante();
-        estudante1.setNome("Camila");
-        estudante1.setEndereco("Rua n asfaltada");
-        LocalDate dataNasEstudante1 = LocalDate.of(2000, Month.JUNE, 27);
+        estudante1.setNome("Henrique");
+        estudante1.setEndereco("Rua duque de caxias");
+        LocalDate dataNasEstudante1 = LocalDate.of(1999, Month.MARCH, 23);
         estudante1.setDtNascimento(dataNasEstudante1);
         estudante1.setRa("56498498");
         estudante1.setCurso("TADS");
-        LocalDate dataInicio = LocalDate.of(2022, Month.FEBRUARY, 8);
+        LocalDate dataInicio = LocalDate.of(2019, Month.FEBRUARY, 8);
         estudante1.setDtInicio(dataInicio);
 
+        //Obj Emprestimo
+        Emprestimo emprestimo1 = new Emprestimo();
+        emprestimo1.setEstudante(estudante1);
+        emprestimo1.setFuncionario(func);
+        emprestimo1.setLivro(livroPOO);
+        LocalDate dtEmprestimo = LocalDate.of(2022, Month.AUGUST, 15);
+        LocalDate dtDevolucao = LocalDate.of(2022, Month.SEPTEMBER, 15);
+        emprestimo1.setDtEmprestimo(dtEmprestimo);
+        emprestimo1.setDtDevolucao(dtDevolucao);
+        
     }
 }
